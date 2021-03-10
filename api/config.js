a759039983a6421dc843e072652754374dd1cc2a
José Flores <josecarlosfloresmoran@hotmail.com>
2021-03-09 23:07:20 -0700

    Delete config.js

--- api/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 3000,
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://user:12341234@cluster0.94wpg.mongodb.net/test?retryWrites=true&w=majority',
-    SECRET: process.env.SECRET || 'Evaluacion de Coppel'
-}
